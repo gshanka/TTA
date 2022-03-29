d18e1a50b6444791150df55e4919b83a81715a9b
gshanka <45825149+gshanka@users.noreply.github.com>
2022-03-29 11:07:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,3 +66,22 @@ module "s3_bucket_for_logs" {
 
   attach_elb_log_delivery_policy = true
 }
+resource "aws_kms_key" "imagekmskey" {
+  description             = "encrypt bucket objects"
+  deletion_window_in_days = 5
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mybucket"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.mybucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
