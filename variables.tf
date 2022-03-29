98caaea155ee96bf7ae8f691017f6fb43c6b671e
gshanka <45825149+gshanka@users.noreply.github.com>
2022-03-29 10:28:26 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,3 +27,8 @@ variable "ami_id" {
   description = "AMI id"
   default     = "ami-0b9064170e32bde34"
 }
+
+variable "s3" {
+  description = "S3 Name"
+  default     = "https://elasticbeanstalk-us-east-1-162622478607.s3.amazonaws.com/.elasticbeanstalk"
+}
